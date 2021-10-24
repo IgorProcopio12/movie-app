f19958a39c8782b16fd0bdb0d4876b2c4b8e26ec
Igor Procópio <procopio.igor@hotmail.com>
2021-10-24 15:01:31 -0300

    Update
    Animações campo de texto

--- App.js
+++ App.js
@@ -1,16 +1,117 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
 
 export default function App() {
+  const [offSet] = React.useState(new Animated.ValueXY({ x: 0, y: 95 }));
+  const [opacity] = React.useState(new Animated.Value(0));
+ 
+  React.useEffect(() => {
+    Animated.parallel([
+      Animated.spring(offSet.y, {
+        toValue: 0,
+        speed: 1,
+        bounciness: 15,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+      }),
+    ]).start();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View>
+      
+      </View>
+
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: opacity,
+            transform: [{ translateY: offSet.y }],
+          },
+        ]}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Crie Sua Conta</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#191919",
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+  },
+
+  input: {
+    backgroundColor: "#FFF",
+    width: "90%",
+    marginBottom: 15,
+    color: "#222",
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnSubmit: {
+    backgroundColor: "chocolate",
+    width: "90%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
+  submitText: {
+    color: "#FFF",
+    fontSize: 18,
+  },
+  btnRegister: {
+    marginTop: 10,
+  },
+  registerText: {
+    color: "#FFF",
+  },
+});
